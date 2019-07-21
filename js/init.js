6b0357eeaa1e5787664ad8f802643bd507c02f4f
shashwattt <shashwat1906@gmail.com>
2019-07-21 22:44:15 +0530

    initial commit

---
+++ js/init.js
@@ -0,0 +1,23 @@
+  $(document).ready(function() {
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+      $('.modal').modal();
+      $('select').formSelect();
+      $('.datepicker').datepicker();
+
+      $('input.autocomplete').autocomplete({
+          data: {
+              "Apple": null,
+              "Microsoft": null,
+              "Google": 'https://placehold.it/250x250'
+          },
+      });
+
+      $("#destinationForm").submit(function(event) {
+          window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf('/') + 1) + 'pages/export-service.html'
+      });
+
+      $(".serviceList").click(function(event) {
+          window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf('/') + 1) + 'result.html'
+      });
+  });
